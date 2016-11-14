a55eaabefcb58706ce723b1a7366f6e1286d48a9
Vasanchai <59070156@it.kmitl.ac.th>
2016-11-14 21:02:27 +0700

    Change Code for input graph

--- Graph_job.py
+++ Graph_job.py
@@ -1,13 +1,11 @@
+from openpyxl import load_workbook
 import pygal
 bar_chart = pygal.HorizontalBar()
 bar_chart.title = 'Number of Smoker\n      Jobs'
-bar_chart.add('คนงาน', 1361986)
-bar_chart.add('เกษตร, ประมง', 3715143)
-bar_chart.add('ผู้ปฏิบัติงานด้านความสามารถทางฝีมือ', 1185974)
-bar_chart.add('ผู้ปฏิบัติการเครื่องจักรโรงงาน', 859673)
-bar_chart.add('ผู้บัญญัติกฏหมาย, ข้าราชการระดับผู้อาวุโส', 665292)
-bar_chart.add('ช่างเทคนิค', 232035)
-bar_chart.add('พนักงานบริการ', 535813)
-bar_chart.add('เสมียน', 115913)
-bar_chart.add('ผู้ประกอบวิชาชีพด้านต่างๆ', 106067)
-bar_chart.render_to_file('hello111.svg')
+wb = load_workbook(filename = 'job.xlsx')
+sheet_ranges = wb['Sheet1']
+for j in range(2, 10):
+    cell = 'B'+str(j)
+    cell1 = 'A'+str(j)
+    bar_chart.add(sheet_ranges[cell1].value, sheet_ranges[cell].value)
+bar_chart.render_to_file('project_job.svg')
