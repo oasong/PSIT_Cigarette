a163ba12091efefec3161f1252e9efe08b67dd55
Paroot <59070098@it.kmitl.ac.th>
2016-11-14 19:40:05 +0700

    First Time
    come in and fix it.

---
+++ Graph_job.py
@@ -0,0 +1,12 @@
+import pygal
+bar_chart = pygal.HorizontalBar()
+bar_chart.add('คนงาน', 1361986)
+bar_chart.add('เกษตร, ประมง', 3715143)
+bar_chart.add('ผู้ปฏิบัติงานด้านความสามารถทางฝีมือ', 1185974)
+bar_chart.add('ผู้ปฏิบัติการเครื่องจักรโรงงาน', 859673)
+bar_chart.add('ผู้บัญญัติกฏหมาย, ข้าราชการระดับผู้อาวุโส', 665292)
+bar_chart.add('ช่างเทคนิค', 232035)
+bar_chart.add('พนักงานบริการ', 535813)
+bar_chart.add('เสมียน', 115913)
+bar_chart.add('ผู้ประกอบวิชาชีพด้านต่างๆ', 106067)
+bar_chart.render_to_file('hello111.svg')
